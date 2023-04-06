42778c9103034c1522e78643eeaba491cd0dead3
yhwlee <116032189+yhwlee@users.noreply.github.com>
2023-04-06 12:52:24 +0200

    Updated bot.py to be aware of chat history

--- bot.py
+++ bot.py
@@ -5,8 +5,10 @@ from dotenv import load_dotenv
 import openai
 
 # Load environment variables
+called = False
 load_dotenv()
 conversation = []
+history = []
 
 # Create a new Discord bot instance
 bot = discord.Client(intents=discord.Intents.all())
@@ -16,6 +18,15 @@ DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
 # Set up OpenAI API client
 openai.api_key = os.getenv('OPENAI_API_KEY')
+
+async def fetch_conversation(channel):
+    """Fetches the chat history of the given channel and initializes the conversation list"""
+    global history
+    async for message in channel.history(limit=20):
+        if message.author != bot.user:
+            history.append({"role": "user", "content": message.content})
+        else:
+            history.append({"role": "assistant", "content": message.content})
 
 # Define on_ready event
 @bot.event
@@ -27,14 +38,32 @@ async def on_ready():
 async def on_message(message):
     if message.author == bot.user:
         return
-    conversation.append({"role": "user", "content": message.content})
+    # Fetch the conversation history
+    global called
+    global conversation
+    if not called:
+        await fetch_conversation(message.channel)
+        called = True
+        # Print the conversation history
+        for line in history:
+            print("Role: " + line["role"] + " Content: " + line["content"])
+        conversation = history
+    else:
+        conversation.append({"role": "user", "content": message.content})
     response = openai.ChatCompletion.create(
         model = "gpt-3.5-turbo",
         messages = conversation)
     gpt_response = response.choices[0]["message"]["content"]
     conversation.append({"role": "assistant", "content": gpt_response})
     # Send response to Discord
-    await message.channel.send(gpt_response)
+    if len(gpt_response) > 2000:
+        for i in range(0, len(gpt_response), 2000):
+            if i + 2000 < len(gpt_response):
+                await message.channel.send(gpt_response[i:i+2000])
+            else:
+                await message.channel.send(gpt_response[i:])
+    else:
+        await message.channel.send(gpt_response)
     return
 # Run the Discord bot
 bot.run(DISCORD_TOKEN)
