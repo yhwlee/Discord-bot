7c98129ce9e60966f4cda76aeda3b3ada05a4189
yhwlee <116032189+yhwlee@users.noreply.github.com>
2023-04-14 15:41:36 +0200

    Updated main.py
    No need to run bot twice given its already ran in bot.py file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from bot import bot
 
 if __name__ == '__main__':
-    bot.run(host='localhost', port=8000)
+    print("Ran the Discord bot")
+  #  bot.run(host='localhost', port=8000) No need to run twice (bot.py)
